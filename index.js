9ca20602d5aff9348612d483814164d45ec5ab3c
Cristian <cri.gacitua.flores@gmail.com>
2017-12-20 19:13:48 -0800

    install mongo and mongoose

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 'use strict'
 
 //Instanciar express
+//npm i -S express
 const express = require('express')
+//Instanciar bodyParser
+//npm i -S body-parser
+const bodyParser = require('body-parser')
+
+//sudo npm i -S mongoose
+const mongoose = require('mongoose')
+
+//sudo npm i -D nodemon
 
 const app = express()
+const port = process.env.PORT || 3000
 
-//Puerto a escuchar
-app.listen(3000, () => {
-    console.log('API REST CORRIENDO HTTP');
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+app.get('/api/product', (req,res) => {
+    res.send(200, { products: [] })
 })
+
+app.get('/api/product/:productId', (req,res) => {
+
+})
+
+app.post('/api/product', (req,res) => {
+    console.log(req.body)
+    res.status(200).send({ message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:productId', (req,res) => {
+
+})
+
+app.delete('/api/product/:productId', (req,res) => {
+
+})
+
+mongoose.connect('mongodb://localhost:27017/shopRest', (err, res) => {
+    if (err){
+        return console.log(`Error al conectar a la bd: ${err}`)
+    }
+
+    console.log('Conexion establecida')
+
+    //Puerto a escuchar
+    app.listen(port, () => {
+        console.log('API REST CORRIENDO HTTP');
+    })
+})
+
