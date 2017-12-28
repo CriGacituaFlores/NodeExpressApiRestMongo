5126a188b38628dcce30d2996c456de626e8c2f7
Cristian <cri.gacitua.flores@gmail.com>
2017-12-27 18:11:24 -0800

    dd

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+console.log('AAAAAAAAAAAAAAAAAAAA', process.env.MONGODB)
 module.exports = {
     port: process.env.PORT || 3001,
     db: process.env.MONGODB || 'mongodb://localhost:27017/shopRest',
