6827117b635ec0337a08eae72d7f031723289517
Cristian <cri.gacitua.flores@gmail.com>
2017-12-27 17:30:37 -0800

    añadiendo handlebars

--- app.js
+++ app.js
@@ -6,12 +6,27 @@ const express = require('express')
 //Instanciar bodyParser
 //npm i -S body-parser
 const bodyParser = require('body-parser')
+//sudo npm i -S express-handlebars
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes/index')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+//ficheros con hbs usen hbs
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
 app.use('/api', api)
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.get('/product', (req,res) => {
+    res.render('product')
+})
 
 
 module.exports = app
